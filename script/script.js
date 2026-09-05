ed9328ffbd4fc93e74eaf3c487317d685f1f66ed
Leandro Jesus <pinheiroleandro7009@gmail.com>
2026-09-05 21:14:27 +0100

    feat: first commit build frontend

---
+++ script/script.js
@@ -0,0 +1,15 @@
+let startBtn = document.querySelector('#startBtn');
+let stopBtn = document.querySelector('#stopBtn');
+let resetBtn = document.querySelector('#resetbtn');
+
+startBtn.addEventListener('click', async () => {
+    console.log("ola Mundo");
+});
+
+stopBtn.addEventListener('click', async () => {
+    console.log("stop")
+});
+
+resetBtn.addEventListener('click', async () => {
+    console.log("reset");
+});
